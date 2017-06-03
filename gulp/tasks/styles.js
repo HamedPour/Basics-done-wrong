cd206b24bb3e268c6d46eaf3cafb4958643d21e0
Hamed <hamed_k.pour@yahoo.com>
2017-06-03 14:42:36 +0100

    Completed postcss setup and gulp injection

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,1 +1,24 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+normalize = require('postcss-normalize'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+simpleVars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+hexrgba = require ('postcss-hexrgba'),
+mixins = require('postcss-mixins');
+
+
+// =============================================================================
+
+gulp.task('styles', function(){
+  var plugins = [cssImport, simpleVars, normalize, hexrgba, autoprefixer, nested, mixins]
+  return gulp.src('./app/assets/styles/master.css')
+      .pipe(postcss(plugins))
+      .on('error', function(errorinfo){
+        console.log(errorinfo.toString());
+        this.emit('end')
+      })
+      .pipe(gulp.dest('./app/build/styles'))
+});
+// =============================================================================
